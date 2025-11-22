d3ca7f6062cffd58c33c3b8e0fa185cfffd27a50
Taha Shah <taha.sh4h@gmail.com>
2025-11-21 21:28:14 -0500

    fixed callback

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     (accessToken, refreshToken, profile, done) => {
       // In production persist user profile in database
